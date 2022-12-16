cf2d540fe842d472e9f70f8c129734118d502313
illuz <iilluzen@gmail.com>
2022-12-16 22:02:42 +0800

    recover centos

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -1,73 +1,60 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-export LANG=en_US.UTF-8
+LANG=en_US.UTF-8
 
-# RED='\e[1;31m'    # 红色
-# GREEN='\e[1;32m'  # 绿色
-# YELLOW='\e[1;33m' # 黄色
-# BLUE='\e[1;34m'   # 蓝色
-# PURPLE='\e[1;35m' # 紫色
-# CYAN='\e[1;36m'   # 蓝绿色
-# WHITE='\e[1;37m'  # 白色
-# NC='\e[0m' # 没有颜色
-
-if grep -Eq "Debian" /etc/*-release; then
-    ln -sf /bin/bash /bin/sh
+if [ ! -f /usr/bin/applydeltarpm ];then
+	yum -y provides '*/applydeltarpm'
+	yum -y install deltarpm
 fi
 
-__GET_BIT=`getconf LONG_BIT`
-if [ "$__GET_BIT" == "32" ];then
-	# install rust | 32bit need
-	# curl https://sh.rustup.rs -sSf | sh
-	apt install -y rustc
-fi
+VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
 
-# synchronize time first
-apt-get install ntpdate -y
-ntpdate time.nist.gov | logger -t NTP
+setenforce 0
+sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config
 
-apt update -y
-apt-get update -y 
+yum install -y wget lsof crontabs
+yum install -y python3-devel
+yum install -y python3-pip
+yum install -y python-devel
+yum install -y vixie-cron
+yum install -y curl-devel libmcrypt libmcrypt-devel
+yum install -y mysql-devel
+yum install -y expect
 
 
-apt install -y wget curl lsof unzip
-apt install -y python3-pip
-apt install -y python3-dev
-apt install -y python3-venv
-apt install -y cron
-apt install -y expect
 
-apt install -y locate
-locale-gen en_US.UTF-8
-localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
-
+#https need
 if [ ! -d /root/.acme.sh ];then	
-	curl  https://get.acme.sh | sh
+	curl https://get.acme.sh | sh
 fi
 
-if [ -f /usr/sbin/ufw ];then
+if [ -f /etc/init.d/iptables ];then
 
-	ufw allow 22/tcp
-	ufw allow 80/tcp
-	ufw allow 443/tcp
-	ufw allow 888/tcp
-	# ufw allow 7200/tcp
-	# ufw allow 3306/tcp
-	# ufw allow 30000:40000/tcp
+	iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
+	iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
+	iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
+	iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 888 -j ACCEPT
+	# iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 7200 -j ACCEPT
+	# iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 3306 -j ACCEPT
+	# iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 30000:40000 -j ACCEPT
+	service iptables save
 
+	iptables_status=`service iptables status | grep 'not running'`
+	if [ "${iptables_status}" == '' ];then
+		service iptables restart
+	fi
+
+	#安装时不开启
+	service iptables stop
 fi
 
-if [ -f /usr/sbin/ufw ];then
 
-	ufw disable
-
-fi
-
-
-if [ ! -f /usr/sbin/ufw ];then
-	apt install -y firewalld
+if [ ! -f /etc/init.d/iptables ];then
+	yum install firewalld -y
 	systemctl enable firewalld
+	#取消服务锁定
+	systemctl unmask firewalld
 	systemctl start firewalld
 
 	firewall-cmd --permanent --zone=public --add-port=22/tcp
@@ -78,120 +65,85 @@ if [ ! -f /usr/sbin/ufw ];then
 	# firewall-cmd --permanent --zone=public --add-port=3306/tcp
 	# firewall-cmd --permanent --zone=public --add-port=30000-40000/tcp
 
-	# fix:debian10 firewalld faq
-	# https://kawsing.gitbook.io/opensystem/andoid-shou-ji/untitled/fang-huo-qiang#debian-10-firewalld-0.6.3-error-commandfailed-usrsbinip6tablesrestorewn-failed-ip6tablesrestore-v1.8
-	sed -i 's#IndividualCalls=no#IndividualCalls=yes#g' /etc/firewalld/firewalld.conf
 
+	sed -i 's#AllowZoneDrifting=yes#AllowZoneDrifting=no#g' /etc/firewalld/firewalld.conf
 	firewall-cmd --reload
+	#安装时不开启
+	systemctl stop firewalld
 fi
 
-#安装时不开启
-systemctl stop firewalld
+yum groupinstall -y "Development Tools"
+yum install -y epel-release
 
-
-#fix zlib1g-dev fail
-echo -e "\e[0;32mfix zlib1g-dev install question start\e[0m"
-Install_TmpFile=/tmp/debian-fix-zlib1g-dev.txt
-apt install -y zlib1g-dev > ${Install_TmpFile}
+yum install -y oniguruma oniguruma-devel
+#centos8 stream | use dnf
 if [ "$?" != "0" ];then
-	ZLIB1G_BASE_VER=$(cat ${Install_TmpFile} | grep zlib1g | awk -F "=" '{print $2}' | awk -F ")" '{print $1}')
-	ZLIB1G_BASE_VER=`echo ${ZLIB1G_BASE_VER} | sed "s/^[ \s]\{1,\}//g;s/[ \s]\{1,\}$//g"`
-	# echo "1${ZLIB1G_BASE_VER}1"
-	echo -e "\e[1;31mapt install zlib1g=${ZLIB1G_BASE_VER} zlib1g-dev\e[0m"
-	echo "Y" | apt install zlib1g=${ZLIB1G_BASE_VER}  zlib1g-dev
-fi
-rm -rf ${Install_TmpFile}
-echo -e "\e[0;32mfix zlib1g-dev install question end\e[0m"
-
-
-#fix libunwind-dev fail
-echo -e "\e[0;32mfix libunwind-dev install question start\e[0m"
-Install_TmpFile=/tmp/debian-fix-libunwind-dev.txt
-apt install -y libunwind-dev > ${Install_TmpFile}
-if [ "$?" != "0" ];then
-	liblzma5_BASE_VER=$(cat ${Install_TmpFile} | grep liblzma-dev | awk -F "=" '{print $2}' | awk -F ")" '{print $1}')
-	liblzma5_BASE_VER=`echo ${liblzma5_BASE_VER} | sed "s/^[ \s]\{1,\}//g;s/[ \s]\{1,\}$//g"`
-	echo -e "\e[1;31mapt install liblzma5=${liblzma5_BASE_VER} libunwind-dev\e[0m"
-	echo "Y" | apt install liblzma5=${liblzma5_BASE_VER} libunwind-dev
-fi
-rm -rf ${Install_TmpFile}
-echo -e "\e[0;32mfix libunwind-dev install question end\e[0m"
-
-
-apt install -y libvpx-dev 
-apt install -y libxpm-dev
-apt install -y libwebp-dev
-apt install -y libfreetype6-dev
-
-sudo localedef -i en_US -f UTF-8 en_US.UTF-8
-
-VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
-if [ "$VERSION_ID" == "9" ];then
-	sed "s/flask==2.0.3/flask==1.1.1/g" -i /www/server/mdserver-web/requirements.txt
-	sed "s/cryptography==3.3.2/cryptography==2.5/g" -i /www/server/mdserver-web/requirements.txt
-	sed "s/configparser==5.2.0/configparser==4.0.2/g" -i /www/server/mdserver-web/requirements.txt
-	sed "s/flask-socketio==5.2.0/flask-socketio==4.2.0/g" -i /www/server/mdserver-web/requirements.txt
-	sed "s/python-engineio==4.3.2/python-engineio==3.9.0/g" -i /www/server/mdserver-web/requirements.txt
-	# pip3 install -r /www/server/mdserver-web/requirements.txt
-fi
-
-apt install -y devscripts
-apt install -y net-tools
-apt install -y python3-dev
-apt install -y autoconf
-apt install -y gcc
-
-apt install -y libffi-dev
-apt install -y cmake automake make
-
-apt install -y webp scons
-apt install -y libwebp-dev
-apt install -y lzma lzma-dev
-apt install -y libunwind-dev
-
-apt install -y libpcre3 libpcre3-dev 
-apt install -y openssl
-apt install -y libssl-dev
-
-apt install -y libmemcached-dev
-apt install -y libsasl2-dev
-apt install -y imagemagick 
-apt install -y libmagickwand-dev
-
-apt install -y libxml2 libxml2-dev libbz2-dev libmcrypt-dev libpspell-dev librecode-dev
-apt install -y libgmp-dev libgmp3-dev libreadline-dev libxpm-dev
-apt install -y dia pkg-config
-apt install -y zlib1g-dev
-apt install -y libjpeg-dev libpng-dev
-apt install -y libfreetype6
-apt install -y libjpeg62-turbo-dev
-apt install -y libfreetype6-dev
-apt install -y libevent-dev libncurses5-dev libldap2-dev
-apt install -y libzip-dev
-apt install -y libicu-dev
-
-apt install -y build-essential
-
-apt install -y libcurl4-openssl-dev
-apt install -y curl libcurl4-gnutls-dev
-#https://blog.csdn.net/qq_36228377/article/details/123154344
-# ln -s  /usr/include/x86_64-linux-gnu/curl  /usr/include/curl
-if [ ! -d /usr/include/curl ];then
-    ln -s  /usr/include/x86_64-linux-gnu/curl  /usr/include/curl
+	yum install -y dnf dnf-plugins-core
+	dnf config-manager --set-enabled powertools
+	yum install -y oniguruma oniguruma-devel
+	dnf upgrade -y libmodulemd
 fi
 
 
-apt install -y graphviz bison re2c flex
-apt install -y libsqlite3-dev
-apt install -y libonig-dev
+yum install -y libtirpc libtirpc-devel
+yum install -y rpcgen
+yum install -y openldap openldap-devel  
+yum install -y bison re2c
+yum install -y cmake3
+yum install -y autoconf
+yum install -y make cmake gcc gcc-c++
 
-apt install -y perl g++ libtool    
-apt install -y libxslt1-dev
+yum install -y libmemcached libmemcached-devel
+yum install -y curl curl-devel
+yum install -y zlib zlib-devel
+yum install -y libzip libzip-devel
+yum install -y pcre pcre-devel
+yum install -y icu libicu-devel 
+yum install -y freetype freetype-devel
+yum install -y openssl openssl-devel
+yum install -y libxml2 libxml2-devel
+yum install -y graphviz
+yum install -y sqlite-devel
+yum install -y oniguruma oniguruma-devel
+yum install -y ImageMagick ImageMagick-devel
 
-apt install -y libmariadb-dev
-#apt install -y libmysqlclient-dev   
-apt install -y libmariadb-dev-compat
-#apt install -y libmariadbclient-dev
-    
+
+yum install -y libzstd-devel
+yum install -y libevent libevent-devel unzip zip
+# yum install -y python-imaging
+yum install -y libicu-devel  bzip2-devel  pcre pcre-devel
+
+yum install -y gd gd-devel
+yum install -y libjpeg-devel libpng-devel libwebp libwebp-devel
+
+yum install -y net-tools
+yum install -y ncurses-devel
+yum install -y libXpm-devel
+
+
+for yumPack in flex file libtool libtool-libs kernel-devel patch wget glib2 glib2-devel tar bzip2 bzip2-devel libevent libevent-devel ncurses ncurses-devel curl curl-devel libcurl libcurl-devel e2fsprogs e2fsprogs-devel libidn libidn-devel vim-minimal gettext gettext-devel ncurses-devel gmp-devel libcap diffutils ca-certificates net-tools psmisc git-core c-ares-devel libicu-devel libxslt libxslt-devel zip unzip glibc.i686 libstdc++.so.6 cairo-devel ncurses-devel libaio-devel perl perl-devel perl-Data-Dumper expat-devel readline-devel;
+do yum -y install $yumPack;done
+
+
+if [ "$VERSION_ID" -eq "8" ];then
+	dnf upgrade -y libmodulemd
+fi
+
+if [ "$VERSION_ID" -eq "9" ];then
+	yum install -y patchelf
+	dnf --enablerepo=crb install -y libtirpc-devel
+	dnf --enablerepo=crb install -y libmemcached libmemcached-devel
+	dnf --enablerepo=crb install -y libtool libtool-libs
+	dnf --enablerepo=crb install -y gnutls-devel
+	dnf --enablerepo=crb install -y mysql-devel
+
+	dnf --enablerepo=crb install -y libvpx-devel libXpm-devel libwebp libwebp-devel
+	dnf --enablerepo=crb install -y libjpeg-devel libpng-devel
+	dnf --enablerepo=crb install -y oniguruma oniguruma-devel
+	dnf --enablerepo=crb install -y libzip libzip-devel
+	# yum remove -y chardet
+fi
+
+
 cd /www/server/mdserver-web/scripts && bash lib.sh
 chmod 755 /www/server/mdserver-web/data

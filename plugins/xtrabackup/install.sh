09e6af997ca7d04d5034101ec1414708ea97d627
colin <colin@local.debian>
2022-12-19 16:25:57 +0800

    xtrabackup

--- plugins/xtrabackup/install.sh
+++ plugins/xtrabackup/install.sh
@@ -15,44 +15,30 @@ VERSION=$2
 Install_redis()
 {
 	echo '正在安装脚本文件...' > $install_tmp
-	mkdir -p $serverPath/source
-
-	if [ ! -f $serverPath/source/redis-${VERSION}.tar.gz ];then
-		wget -O $serverPath/source/redis-${VERSION}.tar.gz http://download.redis.io/releases/redis-${VERSION}.tar.gz
-	fi
-	
-	cd $serverPath/source && tar -zxvf redis-${VERSION}.tar.gz
-
-	mkdir -p $serverPath/redis
-	mkdir -p $serverPath/redis/data
-	cd redis-${VERSION} && make PREFIX=$serverPath/redis install
-	sed '/^ *#/d' redis.conf > $serverPath/redis/redis.conf
-
-	if [ -d $serverPath/redis ];then
-		echo "${VERSION}" > $serverPath/redis/version.pl
-		echo '安装完成' > $install_tmp
-
-
-		cd ${rootPath} && python3 ${rootPath}/plugins/redis/index.py start
-		cd ${rootPath} && python3 ${rootPath}/plugins/redis/index.py initd_install
-	fi
+	mkdir -p $serverPath/xtrabackup
+	# arm
+	# wget http://ports.ubuntu.com/pool/universe/p/percona-xtrabackup/percona-xtrabackup_2.4.9-0ubuntu2_arm64.deb
+	# amd
+	wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
+	dpkg -i  percona-xtrabackup_2.4.9-0ubuntu2_arm64.deb
+	apt-get update
+	apt-get -f install
+	apt-get install percona-xtrabackup
+	# apt-get install percona-xtrabackup-24
+	ln -s /www/server/mysql/bin/mysql /usr/bin
+	mkdir -p /var/run/mysqld
+	ln -s /www/server/mysql/mysql.sock /var/run/mysqld/mysqld.sock
+	xtrabackup --version
+	echo "2.4" > $serverPath/xtrabackup/version.pl
+	echo '安装完成' > $install_tmp
 }
 
 Uninstall_redis()
 {
-	if [ -f /usr/lib/systemd/system/redis.service ];then
-		systemctl stop redis
-		systemctl disable redis
-		rm -rf /usr/lib/systemd/system/redis.service
-		systemctl daemon-reload
-	fi
-
-	if [ -f $serverPath/redis/initd/redis ];then
-		$serverPath/redis/initd/redis stop
-	fi
-
-	rm -rf $serverPath/redis
-	echo "Uninstall_redis" > $install_tmp
+	echo '卸载开始' > $install_tmp	
+	apt-get remove percona-xtrabackup* -y
+	rm -rf $serverPath/xtrabackup
+	echo '卸载完成' > $install_tmp	
 }
 
 action=$1

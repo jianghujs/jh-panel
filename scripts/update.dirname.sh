1ac725079613ee7b37a0ae9c32294f9003388acc
colin <colin@localdebian.com>
2022-12-31 16:06:36 +0800

    update.dirname.sh

--- scripts/update.dirname.sh
+++ scripts/update.dirname.sh
@@ -9,18 +9,3 @@ if [ -d "/www/server/mdserver-web" ];then
 fi
 
 cd /www/server/jh-panel && git pull
-
-if [ -f /usr/bin/mw ];then
-	rm -rf /usr/bin/mw
-fi
-
-if [ ! -e /usr/bin/mw ]; then
-	if [ ! -f /usr/bin/mw ];then
-		ln -s /etc/rc.d/init.d/mw /usr/bin/mw
-	fi
-fi
-
-mw 1
-endTime=`date +%s`
-((outTime=($endTime-$startTime)/60))
-echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"

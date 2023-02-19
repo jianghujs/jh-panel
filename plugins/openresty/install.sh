1624e5a798838e95d1f10a514f701b1454344ea1
illuz <iilluzen@gmail.com>
2023-02-19 22:29:11 +0800

    openresty 编译脚本增加 with-stream_realip_module

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -74,7 +74,8 @@ Install_openresty()
 	--with-http_slice_module \
 	--with-http_stub_status_module \
 	--with-http_sub_module \
-	--with-http_realip_module
+	--with-http_realip_module \
+	--with-stream_realip_module
 	# --without-luajit-gc64
 	# --with-debug
 	# 用于调式

a24df59e7dfe4fab545ea4bc6f238006d726276b
illuz <iilluzen@gmail.com>
2023-02-21 22:49:26 +0800

    op_waf 支持 proxy_protocol_addr

--- plugins/op_waf/waf/lua/common.lua
+++ plugins/op_waf/waf/lua/common.lua
@@ -680,6 +680,12 @@ end
 
 function _M.get_real_ip(self, server_name)
     local client_ip = "unknown"
+
+    -- proxy_protocol_addr 优先级最高
+    if not ngx.re.match(ngx.var.proxy_protocol_addr,"\\d+\\.\\d+\\.\\d+\\.\\d+") == nil and self:is_ipaddr(ngx.var.proxy_protocol_addr) then
+        return ngx.var.proxy_protocol_addr
+    end
+
     local site_config = self.site_config
     if site_config[server_name] then
         if site_config[server_name]['cdn'] then

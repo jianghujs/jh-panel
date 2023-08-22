ca0f5056fc6a35c832dacd782f65b8e3763aba32
Jam <yedsn@163.com>
2023-08-22 16:54:28 +0800

    vm os_tool

--- scripts/os_tool/vm/bullseye/index.sh
+++ scripts/os_tool/vm/bullseye/index.sh
@@ -44,37 +44,7 @@ download_and_run() {
 # 根据用户的选择执行对应的操作
 case $choice in
 1)
-    echo "初始化环境"
-    
-    # 定义一个关联数组来存储你的脚本
-    declare -A scripts
-    scripts=(
-    ["分配固定IP"]="set_static_ip.sh"
-    ["配置SSH权限"]="ssh_root_login.sh"
-    ["安装江湖面板"]="install_jhpanel.sh"
-    )
-
-    # 定义一个数组来存储脚本的顺序
-    script_order=("分配固定IP" "配置SSH权限" "安装江湖面板")
-
-    # 创建一个数组，用于dialog的checklist选项
-    script_options=()
-    for key in "${script_order[@]}"; do
-        script_options+=("$key" "" on)
-    done
-
-    cmd=(dialog --separate-output --checklist "请选择要运行的脚本:" 22 76 16)
-    choices=$("${cmd[@]}" "${script_options[@]}" 2>&1 >/dev/tty)
-
-    # 根据用户的选择运行对应的脚本
-    for key in "${script_order[@]}"; do
-        for choice in $choices; do
-            if [[ $key == $choice ]]; then
-                script_file=${scripts[$choice]}
-                download_and_run ${script_file}
-            fi
-        done
-    done
+    download_and_run init_env.sh
     ;;
 2)
     download_and_run ssh_keygen_to_host.sh

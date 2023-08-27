c8f5aec0fba4c0db71a6fa90733af37e395b6958
Jam <yedsn@163.com>
2023-08-27 20:53:31 +0800

    脚本文件固定tmp目录下

--- scripts/os_tool/vm/bullseye/get_migrate_package.sh
+++ scripts/os_tool/vm/bullseye/get_migrate_package.sh
@@ -66,8 +66,8 @@ export LOCAL_IP=$local_ip
 # 下载并执行脚本的函数
 download_and_run() {
     local script_name=$1
-    wget -N -O ./vm/${script_name} ${URLBase}/${script_name}
-    bash ./vm/${script_name}
+    wget -N -O /tmp/vm/${script_name} ${URLBase}/${script_name}
+    bash /tmp/vm/${script_name}
 }
 
 # 根据用户的选择运行对应的脚本

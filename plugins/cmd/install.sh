c269ae480fa6ffde4fdc4d4121bf00b5650a2238
Jam <yedsn@163.com>
2023-09-11 22:05:20 +0800

    脚本管理器删除docker安装脚本

--- plugins/cmd/install.sh
+++ plugins/cmd/install.sh
@@ -21,39 +21,39 @@ Install_cmd()
 	echo '正在安装cmd...' > $install_tmp
 	mkdir -p $serverPath/cmd
 
-	# install cmd
-	apt-get update
-	apt-get install \
-		ca-certificates \
-		curl \
-		gnupg \
-		lsb-release -y
+	# # install cmd
+	# apt-get update
+	# apt-get install \
+	# 	ca-certificates \
+	# 	curl \
+	# 	gnupg \
+	# 	lsb-release -y
 
-	mkdir -p /etc/apt/keyrings
-	rm -rf /etc/apt/keyrings/docker.gpg
-	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	# mkdir -p /etc/apt/keyrings
+	# rm -rf /etc/apt/keyrings/docker.gpg
+	# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-	echo \
-	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	# echo \
+	# "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+	# $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-	apt-get update  
+	# apt-get update  
 
-	chmod a+r /etc/apt/keyrings/docker.gpg
-	apt-get update
+	# chmod a+r /etc/apt/keyrings/docker.gpg
+	# apt-get update
 	
-	ceVersion=$(apt-cache madison docker-ce | grep ${version} | awk '{print $3}')
-	apt-get install docker-ce=${ceVersion} -y --allow-downgrades
+	# ceVersion=$(apt-cache madison docker-ce | grep ${version} | awk '{print $3}')
+	# apt-get install docker-ce=${ceVersion} -y --allow-downgrades
 
-	# install docker-compose
-	if [ -x "$(command -v docker-compose)" ]; then
-		echo "Docker-compose had been installed"
-	else
-		echo "Installing docker-compose..."
-		cp -r ./docker-compose /usr/local/bin/
-		chmod +x /usr/local/bin/docker-compose
-		docker-compose -v
-	fi
+	# # install docker-compose
+	# if [ -x "$(command -v docker-compose)" ]; then
+	# 	echo "Docker-compose had been installed"
+	# else
+	# 	echo "Installing docker-compose..."
+	# 	cp -r ./docker-compose /usr/local/bin/
+	# 	chmod +x /usr/local/bin/docker-compose
+	# 	docker-compose -v
+	# fi
 
 	echo $version > $serverPath/cmd/version.pl
 	echo '安装完成' > $install_tmp
@@ -63,13 +63,13 @@ Uninstall_cmd()
 {
 	echo '正在卸载cmd...' > $install_tmp
 
-	# uninstall docker
-	apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras -y
-	rm -rf /var/lib/docker
-	rm -rf /var/lib/containerd
+	# # uninstall docker
+	# apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras -y
+	# rm -rf /var/lib/docker
+	# rm -rf /var/lib/containerd
 
-	# uninstall docker-compose
-	rm -rf /usr/local/bin/docker-compose
+	# # uninstall docker-compose
+	# rm -rf /usr/local/bin/docker-compose
 
 	rm -rf $serverPath/cmd
 	echo "卸载完成" > $install_tmp

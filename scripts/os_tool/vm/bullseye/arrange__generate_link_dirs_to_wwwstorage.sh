3c6eee082dfa2c39770bf23626951083e03b6eee
Jam <yedsn@163.com>
2023-11-06 23:54:08 +0800

    调整upload软链脚本，有upload软链的目录，同级增加一个multipartTmp的软链

--- scripts/os_tool/vm/bullseye/arrange__generate_link_dirs_to_wwwstorage.sh
+++ scripts/os_tool/vm/bullseye/arrange__generate_link_dirs_to_wwwstorage.sh
@@ -18,84 +18,88 @@ script_file="link_dirs_to_wwwstorage.sh"
 read -p "确定要生成链接${project_dir}下的目录到${storage_dir}的脚本文件吗？（默认y）[y/n]: " choice
 choice=${choice:-"y"}
 
+read -p "请输入需要移动并链接的目录（多个用英文逗号隔开，默认为 upload: " dirs_input
+dirs_input=${dirs_input:-"upload"}
+
+# 将逗号替换为管道，以便在find命令中使用
+IFS=',' read -ra dirs <<< "$dirs_input"
+
 echo "" > $script_file
 
 if [ $choice == "y" ]; then
 
+  # 搜索所有目录，排除"node_modules"、"logs"、"run"目录
+  for cur_dir in "${dirs[@]}"; do
     # 查找project_dir下已存在的目录
     echo "|- 正在查找项目目录下已存在的目录..."
-	while IFS= read -r -d '' dir; do
-		if [[ $dir != *"/app/"* ]]; then
-            # 获取相对路径
-            relative_path="${dir#$storage_dir}"
+    while IFS= read -r -d '' dir; do
+      if [[ $dir != *"/app/"* ]]; then
+              # 获取相对路径
+              relative_path="${dir#$storage_dir}"
 
-            # 构建目标目录路径
-            target_dir="$project_dir$relative_path"    
-            
-            if [ -d "$target_dir" ]; then
-                project_dir_exist_dirs+=("$target_dir")
-            fi
-        fi
-	done < <(find "$storage_dir" -type d \( -name "node_modules" -o -name "logs" -o -name "run" -o -name ".git" \) -prune -o -type d -print0)
-    
+              # 构建目标目录路径
+              target_dir="$project_dir$relative_path"    
+              
+              if [ -d "$target_dir" ]; then
+                  project_dir_exist_dirs+=("$target_dir")
+              fi
+          fi
+    done < <(find "$storage_dir" -type d \( -name "node_modules" -o -name "logs" -o -name "run" -o -name ".git" \) -prune -o -name "$cur_dir"  -type d -print0)
+      
 
-	if [ ${#project_dir_exist_dirs[@]} -gt 0 ]; then
-        echo "检测到${project_dir}下存在以下与${storage_dir}同名的目录："
-        for dir in "${project_dir_exist_dirs[@]}"
-        do
-            echo "- $dir"
-        done
-
-        read -p "要生成删除这些目录的脚本吗？(默认y) [y/n]: " delete_choice
-        delete_choice=${delete_choice:-"y"}
-        if [ $delete_choice == "y" ]; then
+    if [ ${#project_dir_exist_dirs[@]} -gt 0 ]; then
+          echo "检测到${project_dir}下存在以下与${storage_dir}同名的目录："
           for dir in "${project_dir_exist_dirs[@]}"
           do
-			  
-       		  echo "rm -rf \"$dir\""  >> $script_file
-			  echo "|-- 添加 删除${dir}命令成功✅"
+              echo "- $dir"
           done
-        fi
-		
-    fi
 
-    # 搜索所有目录，排除"node_modules"、"logs"、"run"目录
-    find "$storage_dir" -type d \( -name "node_modules" -o -name "logs" -o -name "run" -o -name ".git" \) -prune -o -type d -print | while read dir
-    do
-		if [[ $dir != *"/app/"* ]]; then
-            # 获取相对路径
-            relative_path="${dir#$storage_dir}"
+          read -p "要生成删除这些目录的脚本吗？(默认y) [y/n]: " delete_choice
+          delete_choice=${delete_choice:-"y"}
+          if [ $delete_choice == "y" ]; then
+            for dir in "${project_dir_exist_dirs[@]}"
+            do
+          
+              echo "rm -rf \"$dir\""  >> $script_file
+          echo "|-- 添加 删除${dir}命令成功✅"
+            done
+          fi
+      
+      fi
 
-            # 构建目标目录路径
-            target_dir="$project_dir$relative_path"
-    
-            # 创建软链接
-            echo "ln -s \"$dir\" \"$target_dir\"" >> $script_file
+      # 搜索所有目录，排除"node_modules"、"logs"、"run"目录
+      find "$storage_dir" -type d \( -name "node_modules" -o -name "logs" -o -name "run" -o -name ".git" \) -prune -o -name "$cur_dir"  -type d -print | while read dir
+      do
+      if [[ $dir != *"/app/"* ]]; then
+              # 获取相对路径
+              relative_path="${dir#$storage_dir}"
 
-            echo "|-- 添加 链接${target_dir} 到 ${dir}命令成功✅"
+              # 构建目标目录路径
+              target_dir="$project_dir$relative_path"
+      
+              # 创建软链接
+              echo "ln -s \"$dir\" \"$target_dir\"" >> $script_file
 
-            # 如果是upload，则再增加一个创建multipartTmp目录并软链的命令
-            if [[ $dir == *"/upload/"* ]]; then
-                echo "mkdir -p \"${target_dir/upload/multipartTmp}\"" >> $script_file
-                # 判断是否存在multipartTmp目录
-                if [ -d "${dir/upload/multipartTmp}" ]; then
-                    echo "rsync -a --delete \"${dir/upload/multipartTmp}/\" \"${target_dir/upload/multipartTmp}/\"" >> $script_file
-                fi
-                echo "rm -rf \"${dir/upload/multipartTmp}\"" >> $script_file
-                echo "ln -sf \"${target_dir/upload/multipartTmp}\" \"${dir/upload/multipartTmp}\""  >> $script_file
-                echo "|-- 添加 链接${dir/upload/multipartTmp} 到 ${target_dir/upload/multipartTmp} 命令成功✅"
-                echo "|-- 添加 创建multipartTmp目录命令成功✅"
-            fi
-        fi
-    done
+              echo "|-- 添加 链接${target_dir} 到 ${dir}命令成功✅"
 
-    
-    echo ""
-    echo "===========================生成脚本完成✅=========================="
-    echo "- 项目所在目录：$project_dir"
-    echo "- 项目数据存放目录：$storage_dir"
-    echo "---------------------------------------------------------------"
-	echo "已生成链接${project_dir}下的目录到${storage_dir}的脚本文件： ${script_file}，请手动确认脚本内容并执行该脚本完成操作："
-    echo "bash ${script_file}"
-    echo "==============================================================="
+              # 如果是upload，则再增加一个创建multipartTmp目录并软链的命令
+              if [[ $cur_dir == "upload" ]]; then
+                  echo "mkdir -p \"${dir/upload/multipartTmp}\"" >> $script_file
+                  echo "rm -rf \"${target_dir/upload/multipartTmp}\"" >> $script_file
+                  echo "ln -sf \"${dir/upload/multipartTmp}\" \"${target_dir/upload/multipartTmp}\""  >> $script_file
+                  echo "|-- 添加 链接${target_dir/upload/multipartTmp} 到 ${dir/upload/multipartTmp} 命令成功✅"
+              fi
+          fi
+      done
+
+      
+      echo ""
+      echo "===========================生成脚本完成✅=========================="
+      echo "- 项目所在目录：$project_dir"
+      echo "- 项目数据存放目录：$storage_dir"
+      echo "---------------------------------------------------------------"
+    echo "已生成链接${project_dir}下的目录到${storage_dir}的脚本文件： ${script_file}，请手动确认脚本内容并执行该脚本完成操作："
+      echo "bash ${script_file}"
+      echo "==============================================================="
+  done
 fi

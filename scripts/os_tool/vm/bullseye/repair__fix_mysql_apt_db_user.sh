222fb94488d691830df6d609d505de39f047ab9e
Jam <yedsn@163.com>
2023-12-05 23:30:35 +0800

    将数据库描述信息同步到其他服务器

--- scripts/os_tool/vm/bullseye/repair__fix_mysql_apt_db_user.sh
+++ scripts/os_tool/vm/bullseye/repair__fix_mysql_apt_db_user.sh
@@ -8,4 +8,24 @@
     python3 /www/server/jh-panel/plugins/mysql-apt/index.py sync_get_databases
     python3 /www/server/jh-panel/plugins/mysql-apt/index.py fix_all_db_user
     popd > /dev/null
+
+    # 提示是否rsync同步
+    read -p "需要将数据库信息文件/www/server/mysql-apt/mysql.db同步到其他服务器吗?(默认n)[y/n]" sync
+    sync=${sync:-n}
+
+
+    if [ $sync == "y" ]; then
+        # 输入目标服务器IP
+        read -p "请输入备用服务器IP: " remote_ip
+        if [ -z "$remote_ip" ]; then
+        echo "错误:未指定目标服务器IP"
+        exit 1
+        fi
+
+        # 输入目标服务器SSH端口
+        read -p "请输入备用服务器SSH端口(默认: 10022): " remote_port
+        remote_port=${remote_port:-10022}
+
+        rsync -avu -e "ssh -p ${remote_port}" --progress /www/server/mysql-apt/mysql.db root@${remote_ip}:/www/server/mysql-apt/mysql.db
+    fi
 # fi

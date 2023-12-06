67b39a87cadd0e0baeeb860acb1a95c543bb3745
Jam <yedsn@163.com>
2023-12-06 14:16:10 +0800

    完善脚本mysql状态检查逻辑

--- scripts/os_tool/vm/bullseye/arrange__fix_project_config_use_database_root_user.sh
+++ scripts/os_tool/vm/bullseye/arrange__fix_project_config_use_database_root_user.sh
@@ -5,6 +5,15 @@
 
 # if [ $choice == "y" ]; then
     pushd /www/server/jh-panel > /dev/null
+    if [ ! -d "/www/server/mysql-apt" ]; then
+        echo "检测到未使用mysql-apt"
+        exit 0
+    fi
+    mysql_status=$(python3 /www/server/jh-panel/plugins/mysql-apt/index.py status)
+    if [ $mysql_status != 'start' ]; then
+        echo "检测到mysql-apt已停用，请启用后重试"
+        exit 0
+    fi
     python3 /www/server/jh-panel/scripts/arrange.py fixProjectConfigUseDatabaseRootUser
     popd > /dev/null
 # fi

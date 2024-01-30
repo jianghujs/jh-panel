294a7a70d79ab7053ffb6c11af15fcc42bde5180
illuz <iilluzen@gmail.com>
2024-01-31 00:45:05 +0800

    修复导出数据库数据报错

--- plugins/mysql-apt/index.py
+++ plugins/mysql-apt/index.py
@@ -2403,11 +2403,11 @@ def dumpMysqlData(version=''):
 
     if args['db'].lower() == 'all':
         dlist = findBinlogDoDb()
-        cmd = mysql_dir + "/bin/mysqldump --defaults-file=" + myconf + " " + option + " -uroot -p" + \
+        cmd = mysql_dir + "/bin/usr/bin/mysqldump --defaults-file=" + myconf + " " + option + " -uroot -p" + \
             pwd + " --databases " + \
             ' '.join(dlist) + " | gzip > /tmp/dump.sql.gz"
     else:
-        cmd = mysql_dir + "/bin/mysqldump --defaults-file=" + myconf + " " + option + " -uroot -p" + \
+        cmd = mysql_dir + "/bin/usr/bin/mysqldump --defaults-file=" + myconf + " " + option + " -uroot -p" + \
             pwd + " --databases " + args['db'] + " | gzip > /tmp/dump.sql.gz"
 
     ret = mw.execShell(cmd)

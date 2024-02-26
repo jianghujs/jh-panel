96f8c6a0db88b337217e075e085c4e1436ab5885
Jam <yedsn@163.com>
2024-02-26 22:41:06 +0800

    节流命令池

--- class/plugin/debounce_tool.py
+++ class/plugin/debounce_tool.py
@@ -1,35 +1,24 @@
-import threading
-from functools import wraps
 
-def debounce(delay):
-    """防抖装饰器，以非阻塞方式延迟执行被装饰的函数"""
-    def decorator(func):
-        # 使用字典存储定时器，以支持实例方法
-        timers = {}
-        
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # 使用函数ID和实例ID（如果有）作为键
-            key = (id(func), id(args[0]) if args else None)
-            timer = timers.get(key)
-            
-            # 定义实际执行函数的内部函数
-            def performCall():
-                timers.pop(key, None)  # 执行后从字典中移除定时器
-                # 使用线程执行被装饰的函数
-                thread = threading.Thread(target=func, args=args, kwargs=kwargs)
-                thread.start()
-            
-            # 如果已经有定时器在运行，则取消它
-            if timer is not None:
-                timer.cancel()
-            
-            # 创建并存储一个新的定时器
-            timer = threading.Timer(delay, performCall)
-            timers[key] = timer
-            
-            # 启动定时器
-            timer.start()
-        
-        return wrapper
-    return decorator
+# 节流池文件
+debounce_commands_pool_file = 'data/debounce_commands_pool.json'
+
+# 读取节流池
+def read_debounce_commands_pool():
+    with open(debounce_commands_pool_file, 'r') as file:
+        return json.load(file)
+
+# 写入节流池
+def write_debounce_commands_pool(debounce_commands_pool):
+    with open(debounce_commands_pool_file, 'w') as file:
+        json.dump(debounce_commands_pool, file)
+ 
+# 添加节流命令
+def add_debounce_commands(command, debounce_time):
+    debounce_commands_pool = read_debounce_commands_pool()
+    debounce_commands_pool.append({
+        'command': command,
+        'seconds_to_run': debounce_time,
+        'time': time.time()
+    })
+    write_debounce_commands_pool(debounce_commands_pool)
+    
--- plugins/rsyncd/index.py
+++ plugins/rsyncd/index.py
@@ -13,7 +13,6 @@ from urllib.parse import unquote
 sys.path.append(os.getcwd() + "/class/core")
 sys.path.append(os.getcwd() + "/class/plugin")
 import mw
-from debounce_tool import debounce
 
 app_debug = False
 if mw.isAppleSystem():
@@ -567,7 +566,6 @@ def cmdRecCmd():
 
 # ----------------------------- rsyncdSend start -------------------------
 
-@debounce(delay=10.0)
 def lsyncdReload():
     data = mw.execShell(
         "ps -ef|grep lsyncd |grep -v grep | grep -v python | awk '{print $2}'")

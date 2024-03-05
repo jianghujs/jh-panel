476438c350095e31a35a4f4d014afb281654f035
Jam <yedsn@163.com>
2024-03-05 16:20:53 +0800

    focal和bullseye统一使用default脚本库

--- scripts/os_tool/vm/bullseye/init__install_cn_language.sh
+++
@@ -1,7 +0,0 @@
-# 安装中文环境
-sed -i -e "s/# en_US.UTF-8.*/en_US.UTF-8 UTF-8/" /etc/locale.gen
-sed -i -e "s/# zh_CN.UTF-8.*/zh_CN.UTF-8 UTF-8/" /etc/locale.gen
-sed -i -e "s/# zh_CN.GBK.*/zh_CN.GBK GBK/" /etc/locale.gen
-sed -i -e "s/# zh_CN.GB2312.*/zh_CN.GB2312 GB2312/" /etc/locale.gen
-apt install -y locales ttf-wqy-microhei
-dpkg-reconfigure --frontend=noninteractive locales 

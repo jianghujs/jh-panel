d476b53ff9cd2fc2fbf242e791fe67f6b9bf583e
Jam <yedsn@163.com>
2024-03-29 14:34:52 +0800

    修复lsyncd实时日志文件名异常问题

--- plugins/rsyncd/tool_lsyncd.py
+++ plugins/rsyncd/tool_lsyncd.py
@@ -84,8 +84,8 @@ def updateLsyncdLogCutShell():
     cronInfo['sbody'] = f"""
 #!/bin/bash
 timestamp=$(date +%Y%m%d_%H%M%S)
-cp /www/server/rsyncd/logs/lsyncd.status /www/server/rsyncd/logs/lsyncd_\${{timestamp}}.status
-mv /www/server/rsyncd/logs/lsyncd.log /www/server/rsyncd/logs/lsyncd_\${{timestamp}}.log
+cp /www/server/rsyncd/logs/lsyncd.status /www/server/rsyncd/logs/lsyncd_${{timestamp}}.status
+mv /www/server/rsyncd/logs/lsyncd.log /www/server/rsyncd/logs/lsyncd_${{timestamp}}.log
 
 python3 /www/server/jh-panel/scripts/clean.py /www/server/rsyncd/logs/ '{{"saveAllDay": "3", "saveOther": "1", "saveMaxDay": "30"}}'
   """

dc035ec74ce3dd48ea2639e1138255a858e1715d
Jam <yedsn@163.com>
2025-01-03 13:48:14 +0800

    站点登录

--- class/core/pub_api.py
+++ class/core/pub_api.py
@@ -26,7 +26,7 @@ class pub_api:
         print("site_domain", site_domain)
 
         login_cache_count = 5
-        login_cache_limit = session.get('site_login_cache_limit_' + site_domain, 0)
+        login_cache_limit = session.get('site_login_cache_limit__' + site_domain, 0)
 
         username = request.form.get('username', '').strip()
         password = request.form.get('password', '').strip()
@@ -66,20 +66,22 @@ class pub_api:
             if login_cache_limit >= login_cache_count:
                 return mw.returnJson(False, '登录尝试过多，已暂时锁定!')
             
-            session['site_login_cache_limit_' + site_domain] = login_cache_limit
+            session['site_login_cache_limit__' + site_domain] = login_cache_limit
             return mw.returnJson(False, mw.getInfo("用户名或密码错误,您还可以尝试[{1}]次!", (str(login_cache_count - login_cache_limit))))
 
-        session['site_login_cache_limit_' + site_domain] = None
-        session['site_login_' + site_domain] = True
-        session['site_username_' + site_domain] = username
-        session['site_id_' + site_domain] = site_domain
-        session['site_overdue_' + site_domain] = int(time.time()) + 7 * 24 * 60 * 60
+        session['site_login_cache_limit__' + site_domain] = None
+        session['site_login__' + site_domain] = True
+        session['site_username__' + site_domain] = username
+        session['site_id__' + site_domain] = site_domain
+        session['site_overdue__' + site_domain] = int(time.time()) + 7 * 24 * 60 * 60
 
         return mw.returnJson(True, '登录成功,正在跳转...')
 
     def checkSiteLoginApi(self):
-        site_domain = request.host
-        site_login_key = 'site_login_' + site_domain
+        site_domain = request.host.replace('.', '_')
+        site_login_key = 'site_login__' + site_domain
+
+        print("当前session", session)
 
         if site_login_key in session and session[site_login_key]:
             return ("success", 200)
